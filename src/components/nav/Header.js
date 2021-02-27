ecc06bfc9040a11a0aa737d5a0d34c043862c60f
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-02-27 09:38:36 +0530

    User Logout

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react'
 import { Menu } from 'antd';
-import { AppstoreOutlined, SettingOutlined, UserAddOutlined, UserOutlined } from '@ant-design/icons';
+import { AppstoreOutlined, LogoutOutlined, SettingOutlined, UserAddOutlined, UserOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import firebase from 'firebase';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
     const [current, setCurrent] = useState('home')
 
+    let dispatch = useDispatch()
+    let history = useHistory()
+
     const handleClick = e => {
         setCurrent(e.key)
     };
+
+    const logout = () => {
+        firebase.auth().signOut()
+        dispatch({
+            type: "LOGOUT",
+            payload: null
+        })
+        history.push('/login')
+    }
 
     return (
         <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" theme="dark">
@@ -21,6 +36,7 @@ const Header = () => {
 
                 <Item key="setting:1">Option 1</Item>
                 <Item key="setting:2">Option 2</Item>
+                <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
 
 
             </SubMenu>
