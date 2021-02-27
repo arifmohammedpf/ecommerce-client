524cce91fac250e94828b2009b8fc763a63f0e2a
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-02-27 14:28:54 +0530

    Conditional rendering - No hardcoded username - show login/regstr when no user - show username dropdown when there is user

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -3,7 +3,7 @@ import { Menu } from 'antd';
 import { AppstoreOutlined, LogoutOutlined, SettingOutlined, UserAddOutlined, UserOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import firebase from 'firebase';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 const { SubMenu, Item } = Menu;
@@ -12,6 +12,7 @@ const Header = () => {
     const [current, setCurrent] = useState('home')
 
     let dispatch = useDispatch()
+    let { user } = useSelector((state) => ({ ...state }))
     let history = useHistory()
 
     const handleClick = e => {
@@ -32,22 +33,23 @@ const Header = () => {
             <Item key="home" icon={<AppstoreOutlined />}>
                 <Link to="/">Home</Link>
             </Item>
-            <SubMenu icon={<SettingOutlined />} title="User">
-
-                <Item key="setting:1">Option 1</Item>
-                <Item key="setting:2">Option 2</Item>
-                <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
-
-
-            </SubMenu>
-            <Item key="login" icon={<UserOutlined />} >
-                <Link to="/login">Login</Link>
-            </Item>
-            <Item key="register" icon={<UserAddOutlined />}>
-                <Link to="/register">Register</Link>
-            </Item>
-
-
+            {user && (
+                <SubMenu icon={<SettingOutlined />} title={user.email && user.email.split('@')[0]}>
+                    <Item key="setting:1">Option 1</Item>
+                    <Item key="setting:2">Option 2</Item>
+                    <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
+                </SubMenu>
+            )}
+            {!user && (
+                <Item key="login" icon={<UserOutlined />} >
+                    <Link to="/login">Login</Link>
+                </Item>
+            )}
+            {!user && (
+                <Item key="register" icon={<UserAddOutlined />}>
+                    <Link to="/register">Register</Link>
+                </Item>
+            )}
         </Menu>
     )
 }
