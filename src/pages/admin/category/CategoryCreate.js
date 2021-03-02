954686d3b0a67caa1190e2d9d71ecdf044a8e55a
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-02 23:37:15 +0530

    58. Category - Create and List

--- src/pages/admin/category/CategoryCreate.js
+++ src/pages/admin/category/CategoryCreate.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AdminNav from '../../../components/nav/AdminNav'
+import { toast } from 'react-toastify'
+import { useSelector } from 'react-redux'
+import { createCategory, getCategories, removeCategory } from '../../../functions/category'
 
 const CategoryCreate = () => {
+    const [name, setName] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        loadCategories()
+    }, [])
+
+    const loadCategories = () => {
+        getCategories().then((catg) => setCategories(catg.data))
+    }
+
+    const { user } = useSelector(state => ({ ...state }))
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setLoading(true)
+        createCategory({ name }, user.token)
+            .then(res => {
+                // console.log(res)
+                setLoading(false)
+                setName('')
+                toast.success(`${res.data.name} is created`)
+            })
+            .catch(err => {
+                // console.log(err)
+                setLoading(false)
+                if (err.response.status === 400) toast.error(err.response.data)
+            })
+    }
+
+    const categoryForm = () => <form onSubmit={handleSubmit}>
+        <div className="form-group">
+            <label>Name</label>
+            <input type="text" className="form-control" onChange={e => setName(e.target.value)} required autoFocus value={name} />
+            <br />
+            <button className='btn button-outline-primary'>Save</button>
+        </div>
+    </form>
+
     return (
         <div className='container-fluid'>
             <div className="row">
@@ -9,7 +52,10 @@ const CategoryCreate = () => {
                     <AdminNav />
                 </div>
                 <div className="col">
-                    Category Create Page
+                    {loading ? <h4 className='text-danger'>Loading...</h4> : <h4>Create category</h4>}
+                    {categoryForm()}
+                    <hr />
+                    {JSON.stringify(categories)}
                 </div>
             </div>
         </div>
