7c5c78510c90d54b94ab8b064abeb2683f47ce04
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-03 00:03:47 +0530

    59. Category- Render and Delete

--- src/pages/admin/category/CategoryCreate.js
+++ src/pages/admin/category/CategoryCreate.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import AdminNav from '../../../components/nav/AdminNav'
 import { toast } from 'react-toastify'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import { createCategory, getCategories, removeCategory } from '../../../functions/category'
 
 const CategoryCreate = () => {
@@ -28,12 +30,33 @@ const CategoryCreate = () => {
                 setLoading(false)
                 setName('')
                 toast.success(`${res.data.name} is created`)
+                loadCategories()
             })
             .catch(err => {
                 // console.log(err)
                 setLoading(false)
                 if (err.response.status === 400) toast.error(err.response.data)
             })
+    }
+
+    const handleRemove = async (slug) => {
+        // let answer = window.confirm('Are you Sure to Delete ?')
+        // console.log(answer)
+        if (window.confirm('Are you Sure to Delete ?')) {
+            setLoading(true)
+            removeCategory(slug, user.token)
+                .then(res => {
+                    setLoading(false)
+                    toast.error(`${res.data.name} is deleted`)
+                    loadCategories()
+                })
+                .catch(err => {
+                    if (err.response.status === 400) {
+                        setLoading(false)
+                        toast.error(err.response.data)
+                    }
+                })
+        }
     }
 
     const categoryForm = () => <form onSubmit={handleSubmit}>
@@ -55,7 +78,13 @@ const CategoryCreate = () => {
                     {loading ? <h4 className='text-danger'>Loading...</h4> : <h4>Create category</h4>}
                     {categoryForm()}
                     <hr />
-                    {JSON.stringify(categories)}
+                    {categories.map((catg) => (
+                        <div className='alert alert-secondary' key={catg._id}>
+                            {catg.name}
+                            <span onClick={() => handleRemove(catg.slug)} className='btn btn-sm float-right'><DeleteOutlined className='text-danger' /></span>
+                            <Link to={`/admin/category/${catg.slug}`}><span className='btn btn-sm float-right'><EditOutlined className='text-warning' /></span></Link>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
