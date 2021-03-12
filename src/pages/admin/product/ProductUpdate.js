a41cba6d04c53c028f6f3af4581e9c3803e88fdf
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-12 08:43:15 +0530

    100. Product update Page

---
+++ src/pages/admin/product/ProductUpdate.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import AdminNav from "../../../components/nav/AdminNav";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { createProduct } from "../../../functions/product";
+import ProductCreateForm from "../../../components/form/ProductCreateForm";
+import { getCategories, getCategorySubs } from "../../../functions/category";
+import FileUpload from "../../../components/form/FileUpload";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const ProductUpdate = () => {
+  const [loading, setLoading] = useState(false);
+
+  //redux
+  const { user } = useSelector((state) => ({ ...state }));
+
+  return (
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-2'>
+          <AdminNav />
+        </div>
+        <div className='col-md-10'>
+          <h4>Product Update</h4>
+          <hr />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductUpdate;
