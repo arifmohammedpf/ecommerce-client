fb1423fd436d1e0aa9b939129b162adc1ebbf1f8
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-12 19:44:06 +0530

    102. Get Product on Update Page

--- src/pages/admin/product/ProductUpdate.js
+++ src/pages/admin/product/ProductUpdate.js
@@ -2,19 +2,44 @@ import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
-import { createProduct } from "../../../functions/product";
-import ProductCreateForm from "../../../components/form/ProductCreateForm";
+import { getProduct } from "../../../functions/product";
 import { getCategories, getCategorySubs } from "../../../functions/category";
 import FileUpload from "../../../components/form/FileUpload";
 import { LoadingOutlined } from "@ant-design/icons";
 
+const initialState = {
+  title: "",
+  description: "",
+  price: "",
+  categories: [],
+  category: "",
+  subs: [],
+  shipping: "",
+  quantity: "",
+  images: [],
+  colors: ["Black", "Brown", "Silver", "White", "Blue"],
+  brands: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS"],
+  color: "",
+  brand: "",
+};
+
 const ProductUpdate = ({ match }) => {
   //to get slug, can use useParams from react-router-dom or {match} with props (udemy lec.101)
   //we get match from props since entire app is wrapped in browserRouter. json.stringify(props) for more info
+  const [values, setValues] = useState(initialState);
   const [loading, setLoading] = useState(false);
-
   //router
   const { slug } = match.params;
+
+  useEffect(() => {
+    loadProduct();
+  }, []);
+
+  const loadProduct = () => {
+    getProduct(slug).then((p) => {
+      setValues({ ...values, ...p.data });
+    });
+  };
 
   //redux
   const { user } = useSelector((state) => ({ ...state }));
@@ -27,6 +52,7 @@ const ProductUpdate = ({ match }) => {
         </div>
         <div className='col-md-10'>
           <h4>Product Update</h4>
+          {JSON.stringify(values)}
           <hr />
         </div>
       </div>
