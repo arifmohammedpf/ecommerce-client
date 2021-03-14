9841497f0deaca1fc62751ad7ade1bfd5fca1aa5
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-14 08:09:56 +0530

    Sec 12: Displaying Product Pagination and Carousel ---- 111. Fetch Products in Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getProductsByCount } from "../functions/product";
 
 const Home = () => {
-    return (
-        <div>
-            Home Page
-        </div>
-    )
-}
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-export default Home
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = () => {
+    getProductsByCount(3).then((res) => {
+      setProducts(res.data);
+    });
+  };
+
+  return (
+    <div>
+      Home Page
+      {JSON.stringify(products)}
+    </div>
+  );
+};
+
+export default Home;
