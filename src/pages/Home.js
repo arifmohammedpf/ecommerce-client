63b84b7173b7740bfc8dcb84578db76d4e7cb547
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-17 20:42:31 +0530

    112. Product Card Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ProductCard from "../components/cards/ProductCard";
 import { getProductsByCount } from "../functions/product";
 
 const Home = () => {
@@ -10,16 +11,28 @@ const Home = () => {
   }, []);
 
   const loadAllProducts = () => {
+    setLoading(true);
     getProductsByCount(3).then((res) => {
       setProducts(res.data);
+      setLoading(false);
     });
   };
 
   return (
-    <div>
-      Home Page
-      {JSON.stringify(products)}
-    </div>
+    <>
+      <div className='jumbotron'>
+        {loading ? <h4>Loading...</h4> : <h4>All Products</h4>}
+      </div>
+      <div className='container'>
+        <div className='row'>
+          {products.map((product) => (
+            <div className='col-md-4' key={product._id}>
+              <ProductCard product={product} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
