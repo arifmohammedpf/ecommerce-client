89223c986f96484b1388a567c154fd17b726a35b
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-18 08:55:26 +0530

    117. New Arrivals

---
+++ src/components/Home/NewArrivals.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import ProductCard from "../cards/ProductCard";
+import SkeletonCard from "../cards/SkeletonCard";
+import { getProducts } from "../../functions/product";
+
+const NewArrivals = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = () => {
+    setLoading(true);
+    //sort,order,limit as paramters for getProducts
+    getProducts("createdAt", "desc", 3).then((res) => {
+      setProducts(res.data);
+      setLoading(false);
+    });
+  };
+
+  return (
+    <>
+      <div className='container'>
+        {loading ? (
+          <SkeletonCard count={3} />
+        ) : (
+          <div className='row'>
+            {products.map((product) => (
+              <div className='col-md-4' key={product._id}>
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default NewArrivals;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,44 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
-import ProductCard from "../components/cards/ProductCard";
-import SkeletonCard from "../components/cards/SkeletonCard";
-import { getProductsByCount } from "../functions/product";
+import NewArrivals from "../components/Home/NewArrivals";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    loadAllProducts();
-  }, []);
-
-  const loadAllProducts = () => {
-    setLoading(true);
-    getProductsByCount(3).then((res) => {
-      setProducts(res.data);
-      setLoading(false);
-    });
-  };
-
   return (
     <>
       <div className='jumbotron text-danger h1 font-weight-bold text-center'>
         {/* {loading ? <h4>Loading...</h4> : <h4>All Products</h4>} */}
         <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
       </div>
-      <div className='container'>
-        {loading ? (
-          <SkeletonCard count={3} />
-        ) : (
-          <div className='row'>
-            {products.map((product) => (
-              <div className='col-md-4' key={product._id}>
-                <ProductCard product={product} />
-              </div>
-            ))}
-          </div>
-        )}
+      <div className='text-center p-3 mt-5 display-3 jumbotron'>
+        New Arrivals
       </div>
+      <NewArrivals />
     </>
   );
 };
