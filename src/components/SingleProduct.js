cc0fa88c2a160dda5cda6fea0fb94d567dcfe205
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-19 08:11:58 +0530

    130. React Star Rating

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -6,11 +6,12 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Laptop from "../images/laptop.jpg";
 import ProductListItems from "./cards/ProductListItems";
+import StarRating from "react-star-ratings";
 
 const { TabPane } = Tabs;
 
 const SingleProduct = ({ product }) => {
-  const { title, images, description } = product;
+  const { title, images, description, _id } = product;
   return (
     <>
       <div className='col-md-7'>
@@ -36,6 +37,16 @@ const SingleProduct = ({ product }) => {
       </div>
       <div className='col-md-5'>
         <h1 className='bg-info p-3'>{title}</h1>
+        <StarRating
+          name={_id}
+          numberOfStars={5}
+          rating={2}
+          changeRating={(newRating, name) => {
+            console.log(newRating, name);
+          }}
+          isSelectable={true}
+          starRatedColor='red'
+        />
         <Card
           actions={[
             <>
