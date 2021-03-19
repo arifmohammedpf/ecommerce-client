b909a4db08e7c2627bc58adb00716fbede8af0f0
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-19 10:46:54 +0530

    133. Redirect to Intended Page

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,116 +1,167 @@
-import React, { useEffect, useState } from 'react'
-import { auth, googleAuthProvider } from '../../firebase'
-import { toast } from 'react-toastify'
-import { Button } from 'antd';
-import { GoogleOutlined, MailOutlined } from '@ant-design/icons';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { createOrUpdateUser } from '../../functions/auth'
+import React, { useEffect, useState } from "react";
+import { auth, googleAuthProvider } from "../../firebase";
+import { toast } from "react-toastify";
+import { Button } from "antd";
+import { GoogleOutlined, MailOutlined } from "@ant-design/icons";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { createOrUpdateUser } from "../../functions/auth";
 
 const Login = ({ history }) => {
-    const [email, setEmail] = useState('arif.kme16cs012@gmail.com')
-    const [password, setPassword] = useState('udemyecommerce')
-    const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState("arif.kme16cs012@gmail.com");
+  const [password, setPassword] = useState("udemyecommerce");
+  const [loading, setLoading] = useState(false);
 
-    const { user } = useSelector((state) => ({ ...state }))
+  const { user } = useSelector((state) => ({ ...state }));
 
-    useEffect(() => {
-        if (user && user.token) {
-            history.push('/')
-        }
-    }, [user, history])
+  useEffect(() => {
+    let intended = history.location.state;
+    if (intended) return;
+    if (user && user.token) history.push("/");
+  }, [user, history]);
 
-    let dispatch = useDispatch()
+  let dispatch = useDispatch();
 
-    const roleBasedRedirect = (res) => {
-        if (res.data.role === 'admin') {
-            history.push('/admin/dashboard')
-        } else {
-            history.push('/user/history')
-        }
+  const roleBasedRedirect = (res) => {
+    //check if intended
+    let intended = history.location.state;
+    if (intended) {
+      history.push(intended.from);
+    } else {
+      if (res.data.role === "admin") {
+        history.push("/admin/dashboard");
+      } else {
+        history.push("/user/history");
+      }
     }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        setLoading(true)
-        try {
-            const result = await auth.signInWithEmailAndPassword(email, password)
-            const { user } = result
-            const idTokenResult = await user.getIdTokenResult()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const result = await auth.signInWithEmailAndPassword(email, password);
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
 
-            createOrUpdateUser(idTokenResult.token) //auth check backend function
-                .then((res) => {
-                    dispatch({
-                        type: "LOGGED_IN_USER",
-                        payload: {
-                            name: res.data.name,
-                            email: res.data.email,
-                            token: idTokenResult.token,
-                            role: res.data.role,
-                            _id: res.data._id,
-                        }
-                    })
-                    roleBasedRedirect(res)
-                })
-                .catch(err => console.log(err))
-            // history.push('/')            
-        } catch (error) {
-            console.log(error);
-            toast.error(error.message)
-            setLoading(false)
-        }
-
+      createOrUpdateUser(idTokenResult.token) //auth check backend function
+        .then((res) => {
+          dispatch({
+            type: "LOGGED_IN_USER",
+            payload: {
+              name: res.data.name,
+              email: res.data.email,
+              token: idTokenResult.token,
+              role: res.data.role,
+              _id: res.data._id,
+            },
+          });
+          roleBasedRedirect(res);
+        })
+        .catch((err) => console.log(err));
+      // history.push('/')
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+      setLoading(false);
     }
+  };
 
-    const googleLogin = async () => {
-        auth.signInWithPopup(googleAuthProvider)
-            .then(async (result) => {
-                const { user } = result
-                const idTokenResult = await user.getIdTokenResult()
-                createOrUpdateUser(idTokenResult.token) //auth check backend function
-                    .then((res) => {
-                        dispatch({
-                            type: "LOGGED_IN_USER",
-                            payload: {
-                                name: res.data.name,
-                                email: res.data.email,
-                                token: idTokenResult.token,
-                                role: res.data.role,
-                                _id: res.data._id,
-                            }
-                        })
-                        roleBasedRedirect(res)
-                    })
-                    .catch(err => console.log(err))
-                // history.push('/')
-            })
-            .catch((err) => {
-                console.log(err);
-                toast.error(err.message)
-            })
-    }
+  const googleLogin = async () => {
+    auth
+      .signInWithPopup(googleAuthProvider)
+      .then(async (result) => {
+        const { user } = result;
+        const idTokenResult = await user.getIdTokenResult();
+        createOrUpdateUser(idTokenResult.token) //auth check backend function
+          .then((res) => {
+            dispatch({
+              type: "LOGGED_IN_USER",
+              payload: {
+                name: res.data.name,
+                email: res.data.email,
+                token: idTokenResult.token,
+                role: res.data.role,
+                _id: res.data._id,
+              },
+            });
+            roleBasedRedirect(res);
+          })
+          .catch((err) => console.log(err));
+        // history.push('/')
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error(err.message);
+      });
+  };
 
-    const loginForm = () =>
-        <form onSubmit={handleSubmit}>
-            <div className="form-group"><input type="email" className="form-control" placeholder="Your email" value={email} onChange={e => setEmail(e.target.value)} autoFocus /></div>
-            <div className="form-group"><input type="password" className="form-control" placeholder="Your password" value={password} onChange={e => setPassword(e.target.value)} /></div>
+  const loginForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className='form-group'>
+        <input
+          type='email'
+          className='form-control'
+          placeholder='Your email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          autoFocus
+        />
+      </div>
+      <div className='form-group'>
+        <input
+          type='password'
+          className='form-control'
+          placeholder='Your password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
 
-            <br />
-            <Button onClick={handleSubmit} type='submit' className='mb-3' block shape='round' icon={<MailOutlined />} size="large" disabled={!email || password.length < 6}>Login with Email/Password</Button>
-        </form>
+      <br />
+      <Button
+        onClick={handleSubmit}
+        type='submit'
+        className='mb-3'
+        block
+        shape='round'
+        icon={<MailOutlined />}
+        size='large'
+        disabled={!email || password.length < 6}
+      >
+        Login with Email/Password
+      </Button>
+    </form>
+  );
 
-    return (
-        <div className="container p-5">
-            <div className="row">
-                <div className="col-md-6 offset-3">
-                    {loading ? (<h4 className='text-danger'>Loading...</h4>) : (<h4>Login</h4>)}
-                    {loginForm()}
-                    <Button onClick={googleLogin} type='submit' className='mb-3' block shape='round' icon={<GoogleOutlined />} size="large">Login with Google</Button>
-                    <Link to="/forgot/password" className="text-danger">Forgot Password</Link>
-                </div>
-            </div>
+  return (
+    <div className='container p-5'>
+      <div className='row'>
+        <div className='col-md-6 offset-3'>
+          {loading ? (
+            <h4 className='text-danger'>Loading...</h4>
+          ) : (
+            <h4>Login</h4>
+          )}
+          {loginForm()}
+          <Button
+            onClick={googleLogin}
+            type='submit'
+            className='mb-3'
+            block
+            shape='round'
+            icon={<GoogleOutlined />}
+            size='large'
+          >
+            Login with Google
+          </Button>
+          <Link to='/forgot/password' className='text-danger'>
+            Forgot Password
+          </Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
