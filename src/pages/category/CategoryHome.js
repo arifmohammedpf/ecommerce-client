2a2024f35cdd221aa01c79473f93d0f344062c8b
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-20 17:22:17 +0530

    143. CategoryPage with Products

--- src/pages/category/CategoryHome.js
+++ src/pages/category/CategoryHome.js
@@ -12,13 +12,36 @@ const CategoryHome = ({ match }) => {
 
   useEffect(() => {
     setLoading(true);
-    getCategory(slug).then((c) => {
-      setCategory(c.data);
-      console.log(JSON.stringify(c.data, null, 4));
+    getCategory(slug).then((res) => {
+      //   console.log(JSON.stringify(c.data, null, 4));
+      setCategory(res.data.category);
+      setProducts(res.data.products);
+      setLoading(false);
     });
   }, []);
 
-  return <div>{slug}</div>;
+  return (
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col'>
+          {loading ? (
+            <h4 className='jumbotron text-center p-3 mt-5 mb-5 '>Loading...</h4>
+          ) : (
+            <h4 className='jumbotron text-center p-3 mt-5 mb-5'>
+              {products.length} Products in "{category.name}" category
+            </h4>
+          )}
+        </div>
+      </div>
+      <div className='row'>
+        {products.map((product) => (
+          <div className='col-md-4' key={product._id}>
+            <ProductCard product={product} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryHome;
