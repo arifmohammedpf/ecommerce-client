3169160e4515a7cbbff86edbac8d6fa16db6a1a9
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-20 21:18:36 +0530

    147. SearchFilter- Redux Setup

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,65 +1,85 @@
-import React, { useState } from 'react'
-import { Menu } from 'antd';
-import { AppstoreOutlined, LogoutOutlined, SettingOutlined, UserAddOutlined, UserOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
-import firebase from 'firebase';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { Menu } from "antd";
+import {
+  AppstoreOutlined,
+  LogoutOutlined,
+  SettingOutlined,
+  UserAddOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import firebase from "firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import Search from "../form/Search";
 
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
-    const [current, setCurrent] = useState('home')
+  const [current, setCurrent] = useState("home");
 
-    let dispatch = useDispatch()
-    let { user } = useSelector((state) => ({ ...state }))
-    let history = useHistory()
+  let dispatch = useDispatch();
+  let { user } = useSelector((state) => ({ ...state }));
+  let history = useHistory();
 
-    const handleClick = e => {
-        setCurrent(e.key)
-    };
+  const handleClick = (e) => {
+    setCurrent(e.key);
+  };
 
-    const logout = () => {
-        firebase.auth().signOut()
-        dispatch({
-            type: "LOGOUT",
-            payload: null
-        })
-        history.push('/login')
-    }
+  const logout = () => {
+    firebase.auth().signOut();
+    dispatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+    history.push("/login");
+  };
 
-    return (
-        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" theme="dark">
-            <Item key="home" icon={<AppstoreOutlined />}>
-                <Link to="/">Home</Link>
+  return (
+    <Menu
+      onClick={handleClick}
+      selectedKeys={[current]}
+      mode='horizontal'
+      theme='dark'
+    >
+      <Item key='home' icon={<AppstoreOutlined />}>
+        <Link to='/'>Home</Link>
+      </Item>
+      {user && (
+        <SubMenu
+          icon={<SettingOutlined />}
+          title={user.email && user.email.split("@")[0]}
+        >
+          {user && user.role === "subscriber" && (
+            <Item>
+              <Link to='/user/history'>Dashboard</Link>
             </Item>
-            {user && (
-                <SubMenu icon={<SettingOutlined />} title={user.email && user.email.split('@')[0]}>
-                    {user && user.role === 'subscriber' && (
-                        <Item>
-                            <Link to='/user/history'>Dashboard</Link>
-                        </Item>
-                    )}
-                    {user && user.role === 'admin' && (
-                        <Item>
-                            <Link to='/admin/dashboard'>Dashboard</Link>
-                        </Item>
-                    )}
-                    <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
-                </SubMenu>
-            )}
-            {!user && (
-                <Item key="login" icon={<UserOutlined />} >
-                    <Link to="/login">Login</Link>
-                </Item>
-            )}
-            {!user && (
-                <Item key="register" icon={<UserAddOutlined />}>
-                    <Link to="/register">Register</Link>
-                </Item>
-            )}
-        </Menu>
-    )
-}
+          )}
+          {user && user.role === "admin" && (
+            <Item>
+              <Link to='/admin/dashboard'>Dashboard</Link>
+            </Item>
+          )}
+          <Item icon={<LogoutOutlined />} onClick={logout}>
+            Logout
+          </Item>
+        </SubMenu>
+      )}
+      {!user && (
+        <Item key='login' icon={<UserOutlined />}>
+          <Link to='/login'>Login</Link>
+        </Item>
+      )}
+      {!user && (
+        <Item key='register' icon={<UserAddOutlined />}>
+          <Link to='/register'>Register</Link>
+        </Item>
+      )}
+      <span className='float-right p-1'>
+        <Search />
+      </span>
+    </Menu>
+  );
+};
 
-export default Header
+export default Header;
