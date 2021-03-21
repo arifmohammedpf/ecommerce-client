7007f273635aceb60681cd5a073d717fdb571d81
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-21 12:06:16 +0530

    151. Filter By Price Range Frontend

--- src/components/cards/ProductCard.js
+++ src/components/cards/ProductCard.js
@@ -8,7 +8,7 @@ import { showAverage } from "../../functions/rating";
 const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
-  const { images, title, description, slug } = product;
+  const { images, title, description, slug, price } = product;
   return (
     <>
       {
@@ -42,7 +42,7 @@ const ProductCard = ({ product }) => {
         ]}
       >
         <Meta
-          title={title}
+          title={`${title} - $${price}`}
           description={`${description && description.substring(0, 40)}...`}
         />
       </Card>
