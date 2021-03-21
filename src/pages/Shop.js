43fa4765c23171a79505edc0fb4fa1f076f442f8
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2021-03-21 08:02:28 +0530

    149. Search By Text

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { getProductsByCount } from "../functions/product";
+import {
+  getProductsByCount,
+  fetchProductsByFilter,
+} from "../functions/product";
 import { useSelector, useDispatch } from "react-redux";
 import ProductCard from "../components/cards/ProductCard";
 
@@ -7,15 +10,34 @@ const Shop = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  let { search } = useSelector((state) => ({ ...state }));
+  const { text } = search;
+
   useEffect(() => {
     setLoading(true);
     loadAllProducts();
   }, []);
 
+  //1. load products by default on shopPage load
   const loadAllProducts = () => {
     getProductsByCount(12).then((prod) => {
       setProducts(prod.data);
       setLoading(false);
+    });
+  };
+
+  //2. load products on user input
+  useEffect(() => {
+    // console.log('load products on user input-->',text);
+    const delayed = setTimeout(() => {
+      fetchProducts({ query: text });
+    }, 300);
+    return () => clearTimeout(delayed);
+  }, [text]);
+
+  const fetchProducts = (arg) => {
+    fetchProductsByFilter(arg).then((res) => {
+      setProducts(res.data);
     });
   };
 
